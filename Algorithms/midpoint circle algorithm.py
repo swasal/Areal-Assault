4dfb9e7a7fbdabf248ac840d649de27ba9e9c90a
swasal <144431311+swasal@users.noreply.github.com>
2024-04-05 17:52:30 +0600

    updated readme file and added texample cases for midpointcircle
    added a proper readme file  the basic of the project and added example cases for understanding the midpoint circle function

--- Algorithms/midpoint circle algorithm.py
+++ Algorithms/midpoint circle algorithm.py
@@ -7,6 +7,8 @@ from OpenGL.GLU import *
 
 
 
+
+#midpoint circle algo
 def circlepoints(x, y, a,b, zone=""):
     #list that contains all the points for each zone 0-8
     l=[(x+a,y+b),(y+a,x+b),(y+a,-x+b),(x+a,-y+b),(-x+a,-y+b), (-y+a,-x+b), (-y+a,x+b), (-x+a,y+b)] 
@@ -87,15 +89,84 @@ def drawpoints(points:list, red, green, blue): #uased to draw the points from a 
 
 
 
+
+"""the code for testign the function is given below3 alog witha few example uses"""
+
+
+#testign the code
+
+#Var innit
+bg=0
+circley = 200
+fallSpeed = 0.2
+
+
+
+def fallingcircle():
+    global diamondy
+    points=midpointcircledraw(0,circley,100)
+    drawpoints(points, 1.0,0,1.0)
+
+
+
 def circle():
     #draws a full circle
-    
-    points=midpointcircledraw(4,5,100)
+    points=midpointcircledraw(4,100,50)
     drawpoints(points, 1.0,0,1.0)
 
 
 def customcircle():
 
-    #draws zone 1,2,4,6,7
-    points=midpointcircledraw(0,20,100, 1,2,6,7,4)
+    #draws zone 1,4 and 6
+    points=midpointcircledraw(0,-50,50, 1, 6, 4) #iwrote it as 1, 6, 4 to show that the order doesnt matter
     drawpoints(points, 1.0,0,1.0)
+
+
+def display():
+    #//clear the display
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glClearColor(bg,bg,bg, 0);	#//color black
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    gluLookAt(0,0,200,	0,0,0,	0,1,0)
+    glMatrixMode(GL_MODELVIEW)
+    #draw function calls
+    fallingcircle()
+    customcircle()
+    circle()
+
+    glutSwapBuffers()
+
+
+def animate():
+    global circley, fallspeed
+    circley-=fallSpeed
+    if circley<=-250:
+        circley=250
+    glutPostRedisplay()
+
+
+def init():
+    glClearColor(0,0,0,0)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(104,	1,	1,	1000.0)
+   
+
+
+glutInit()
+glutInitWindowSize(1000, 1000)
+glutInitWindowPosition(500, 0)
+glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB) 
+
+wind = glutCreateWindow(b"Diamond catcher")
+init()
+
+glutDisplayFunc(display)	
+glutIdleFunc(animate)	
+# glutKeyboardFunc(keyboardListener)
+# glutSpecialFunc(specialKeyListener)
+# glutMouseFunc(mouseListener)
+
+glutMainLoop() 
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Areal-Assault-
 A two player game for the cse423 project
+the basic idea of the oproject is [here](https://drive.google.com/file/d/1Wgi0PAkcFC6i9AKqsvrRnFHnEptjfWWW/view?usp=drive_link)
+
+
+## Base concept
+
+The basic idea is a 2 player local multipllayer cannon game. Each player has a cannon that cna fire ata  trajectorory of 90 degrees centered from the top of thier character model. The goal of the game is toi get your enemy to 0 hp. 
+
+### character ability
+The said characters have two abilities a shield and a misile that can be fired horizontally (which can be evaded using the terrain or jumping mechanism).
+
+### controls
+
+each cplayer's character can move sideways and jump and has a shield
+
+
+## files
+
+The repository contains an [algorithm](/Algorithms) folder
+
+Both these algorithms return a list of tuples in which every point is a tuple (x,y). These points from the list are rendered usign the draw function whicvh takes i a list of all the points and then renders them in the display.
+
+The list containing the p[oints looks something like =. [(x1,y1), (x2,y2), (x3,y3), ........]] 
+
+### midpoint line
+
+The [midpoint line file](/Algorithms/midpointlike%20algo.py) contains the algorithm to draw the line using two points x1,1 to x2,y2 and returns a list of all the points
+
+### midpoint circle
+
+The [midpoint circle file](/Algorithms/midpoint%20circle%20algorithm.py) contains the midpoint circle algorithm which can draw either a full circle or specific zones of a circle. The midpointcircle takes in three compulsory parameters, the center of the circle x,y and the radius. The fourth is the zones which if left alone will render a full circle. The tesrt c0odes are provded in the file
+
+paste the l
